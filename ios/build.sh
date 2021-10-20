56b9228dd007e621aae809664654532680bc4f13
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-10-20 11:12:54 +0200

    Improves iOS scripts

--- ios/build.sh
+++ ios/build.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+# #!/usr/bin/env bash
 
 set -e
 
 export CONFIGURATION=debug
 
 if [ ! -d "assets" ]; then
-	echo "Linking 'assets' path to '../assets'"
-	ln -s ../assets assets
+  echo "Linking 'assets' path to '../assets'"
+  ln -s ../assets assets
 fi
 
 echo "Generating XCode project"
 xcodegen generate
 
-echo "Building runst"
-./build_rust_deps.sh
+echo -n $'Do you wish a release? (yes/no)\n'
+read isRelease
+echo "Building rust"
+./build_rust_deps.sh "$isRelease"
